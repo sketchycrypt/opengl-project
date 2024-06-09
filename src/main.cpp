755bf32070e7538c0adf2e31a387e0505078fd45
sketchycrypt <sketchygfx@hotmail.com>
2024-06-09 13:53:37 +0100

    shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <cmath>
+
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
 
 using namespace std;
 
@@ -22,6 +38,12 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    GLfloat vertices[] = {
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+    };
+
     GLFWwindow *window;
     window = glfwCreateWindow(600, 800, "game window", NULL, NULL);
     if (window == NULL)
@@ -37,13 +59,53 @@ int main()
         return -1;
     }
 
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    // wrap shaders
+    GLuint shaderWrapper = glCreateProgram();
+    glAttachShader(shaderWrapper, vertexShader);
+    glAttachShader(shaderWrapper, fragmentShader);
+    glLinkProgram(shaderWrapper);
+
+    //cleanup
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLuint VAO, VBO;
+    
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+
+
     while (!glfwWindowShouldClose(window))
     {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-        glClearColor(1.00f, 0.00f, 0.00f, 1.00f);
+        glClearColor(0.07f, 0.13f, 0.17f, 1.00f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderWrapper);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(window);
         glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+        
+        glfwPollEvents();
     }
 
     glfwTerminate();
